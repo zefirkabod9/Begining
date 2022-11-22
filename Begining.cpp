27e01a8c484c424134555b9ff73696e3fe06e169
zefirkabod9 <zefirkabod9@gmail.com>
2022-11-22 17:57:11 +0200

    Add project files.

---
+++ Begining.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+//cout - вивід у консоль
+//cin - зчитування даних з консолі
+using namespace std;
+
+int main(){
+    int a = 0;
+    int b = 0;
+    cin >> a;
+    cin >> b;
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;
+
+    string name;
+    int age;
+    cin >> name;
+    cin >> age;
+    cout << "My name is " << name << " , im " << age << " years old." << endl;
+}
